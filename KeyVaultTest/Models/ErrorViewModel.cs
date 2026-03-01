ad181b497b6597d0f4afd8b1098f796946abeb3a
praveenkumarkilari83 <sbaiskht@gmail.com>
2026-03-01 15:37:50 -0500

    Initial commit

---
+++ KeyVaultTest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KeyVaultTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
